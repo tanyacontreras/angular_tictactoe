bc48d5b65c82ebaee183c35f88acd5af42ae9974
tanyacontreras <tcedith27@gmail.com>
2014-01-17 15:54:09 -0800

    css, clicks and reset works

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
 function mainCtrl($scope){
-	$scope.gameBoard = [['','',''], ['','',''], ['','','']];
-	$scope.currentPlayer = 'X'; 
-	$scope.playerTurn = function(row, col) {
-		if(!scope.gameBoard[row][col])
-			$scope.gameBoard[][] = currentPlayer
-			cp = currentPlayer == 'x', ? 'o' : 'x' ; 
+	$scope.gameBoard = [['','',''],['','',''],['','','']];
+	$scope.currentPlayer = 'Lola'; 
+	$scope.resetBoard = function() {
+		$scope.gameBoard = [['','',''],['','',''],['','','']];		
 	}
-}
+	$scope.playerTurn = function(row, col) {
+		if(!$scope.gameBoard[row][col])
+			{
+				$scope.gameBoard[row][col] = $scope.currentPlayer;
+				$scope.currentPlayer = ($scope.currentPlayer == 'Lola' ? 'Charlie' : 'Lola');
+			}	
+	};
+};
+
+
