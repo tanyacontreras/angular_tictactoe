de25357424ae4daa8eb4eefc4e6b57a65ecd3d1c
tanyacontreras <tcedith27@gmail.com>
2014-01-23 20:09:03 -0800

    centered reset button, eventually it will only appear during the game but will disappear once there is a cats game or a winner. lookin good kid.

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 function boardCtrl($scope) {
 	$scope.boxes = ["","","","","","","","",""];
 	$scope.turnCounter = 0;
-	$scope.players = [{name: "Charlie", image: "images/charlie.jpg"}, {name: "Lola", image:"images/lola.jpg"}];
+	$scope.players = [{name: "Lola", image:"images/lola.jpg"}, {name: "Charlie", image: "images/charlie.jpg"}];
 	$scope.xTurn = $scope.players[0].image;
 	$scope.win = false;
-	$scope.message = "Ready? Go!"
+	gameOver = false;
+	$scope.message = "Ready? Go!";
 	
 	$scope.takeTurn = function (i) {
 		$scope.message = "";
-		if ($scope.boxes[i] == "") {
+		if ($scope.boxes[i] == "" && gameOver == false) {
 			$scope.boxes[i] = $scope.xTurn;
 			if ($scope.boxes[i] == $scope.players[0].image) {
 				$scope.xTurn = $scope.players[1].image
+				$scope.message = "Charlie's Turn"
 			} else {
 				$scope.xTurn = $scope.players[0].image
+				$scope.message = "Lola's Turn"
 			}
 			$scope.turnCounter++;
 		} else {
-			$scope.message = "Taken!";
+			$scope.message = "Oops! Already Taken!";
 		};
 		if ($scope.turnCounter >= 4) {
 		 	$scope.checkWin();
@@ -29,7 +32,7 @@ function boardCtrl($scope) {
 		$scope.message = "";
 		$scope.win = false;
 		$scope.turnCounter = 0;
-		$scope.message = "Ready? Go!"
+		$scope.message = "Ready...Go!"
 
 	};
 
@@ -37,13 +40,15 @@ function boardCtrl($scope) {
 		$scope.winAry = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 		for (var i = 0; i < 8; i++) {
 			if ($scope.boxes[$scope.winAry[i][0]] == $scope.boxes[$scope.winAry[i][1]] && $scope.boxes[$scope.winAry[i][0]] == $scope.boxes[$scope.winAry[i][2]] && $scope.boxes[$scope.winAry[i][0]] != "") {
-				$scope.message = "Winner!";
+				$scope.message = $scope.players[0].name + ' wins!';
 				$scope.win = true;
+				gameOver = true;
 				break;
 				}
 		};
 			if (!$scope.win && $scope.turnCounter >= 9){
 				$scope.message = "Cat's Game!";
+				gameOver = true;
 			};
 	};
 };
